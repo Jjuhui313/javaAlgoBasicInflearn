a5aefb7b21411f11682c7bb39f636e88823cf595
Jjuhui313 <hyacinth92928@gmail.com>
2024-10-12 16:42:34 +0900

    Feat: 06-05 중복 확인 풀이 완료

---
+++ src/algo06_05/Main.java
@@ -0,0 +1,40 @@
+package algo06_05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+ * 06-05 중복 확인
+ *
+ * HashSet 사용하지 않고 정렬을 이용하여 풀어보기
+ * */
+
+public class Main {
+    public static String solution(int n, int[] arr) {
+        String answer = "U";
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return "D";
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(N, arr));
+
+    }
+}
